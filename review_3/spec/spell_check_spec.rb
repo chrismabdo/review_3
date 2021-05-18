f537ade049faba40f708a0c5badb4e8afb76872c
Chris Abdo <christophermabdo@icloud.com>
2021-05-18 10:37:19 +0100

    red phase of testing a one incorrect word sentence

--- review_3/spec/spell_check_spec.rb
+++ review_3/spec/spell_check_spec.rb
@@ -4,4 +4,8 @@ describe 'spell_check' do
     it 'will return the inputed string' do
         expect(spell_check("", [])).to eq ""
     end
+
+    it 'will return an incorrect string with the correct prefix and suffix chars' do
+        expect(spell_check("dg", ["dog"])).to eq "~dg~"
+    end
 end
