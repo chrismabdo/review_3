183017408336602acbfaf4eb27400498c45d8433
Chris Abdo <christophermabdo@icloud.com>
2021-05-18 10:47:40 +0100

    red phase. testing for empty dictionary with correction of words

--- review_3/spec/spell_check_spec.rb
+++ review_3/spec/spell_check_spec.rb
@@ -8,4 +8,11 @@ describe 'spell_check' do
     it 'will return an incorrect string with the correct prefix and suffix chars' do
         expect(spell_check("dg", ["dog"])).to eq "~dg~"
     end
+
+    it 'will return a flagged string if the dictionary is empty or does not contain correction' do
+        expect(spell_check("dg", [])).to eq "~dg~"
+    end
+    # it 'will return a multiple word sentence inserting ~ in the right places' do
+    #     expect(spell_check("dg ct"))
+    # end
 end
